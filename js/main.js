90188c6997d294961f11a94734335bd5d3eb552b
NoCap3D <116123214+NoCapxGaming@users.noreply.github.com>
2023-02-18 14:47:54 -0400

    testing twitch embed

--- js/main.js
+++ js/main.js
@@ -7,38 +7,10 @@ burger.addEventListener('click', () => {
   burger.classList.toggle('active');
 });
 
-// Initialize variables
-let isDragging = false;
-let startX, startY;
-let scrollLeft, scrollTop;
 
-// Add event listeners
-document.addEventListener('mousedown', e => {
-  const mapImg = document.querySelector('.map-container img');
-  if (e.target === mapImg) {
-    isDragging = true;
-    startX = e.clientX - mapImg.offsetLeft;
-    startY = e.clientY - mapImg.offsetTop;
-    scrollLeft = mapImg.scrollLeft;
-    scrollTop = mapImg.scrollTop;
-    mapImg.style.cursor = 'grabbing';
-  }
-});
+const hideEmbedBtn = document.querySelector('.hide-embed-btn');
+const embedContainer = document.querySelector('.embed-container');
 
-document.addEventListener('mousemove', e => {
-  const mapImg = document.querySelector('.map-container img');
-  if (!isDragging) return;
-  e.preventDefault();
-  const x = e.clientX - mapImg.offsetLeft;
-  const y = e.clientY - mapImg.offsetTop;
-  const walkX = x - startX;
-  const walkY = y - startY;
-  mapImg.scrollLeft = scrollLeft - walkX;
-  mapImg.scrollTop = scrollTop - walkY;
-});
-
-document.addEventListener('mouseup', () => {
-  const mapImg = document.querySelector('.map-container img');
-  isDragging = false;
-  mapImg.style.cursor = 'grab';
+hideEmbedBtn.addEventListener('click', function() {
+  embedContainer.classList.toggle('hidden');
 });
