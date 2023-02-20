cbda95ddf965932503c33562cfebb3f54837de4e
NoCap3D <116123214+NoCapxGaming@users.noreply.github.com>
2023-02-19 20:39:24 -0400

    reusable nav bar has been coded into the site. testing reusable footer.

--- js/main.js
+++ js/main.js
@@ -1,11 +1,46 @@
-const toggleButton = document.getElementById('toggle');
+//reusable navigation bar through the website
+
+const navbarContainer = document.getElementById('navbar-container');
+
+const navbar = document.createElement('nav');
+navbar.className = 'navbar';
+
+const toggleButton = document.createElement('div');
+toggleButton.className = 'navbar-toggle burger';
+toggleButton.id = 'toggle';
+toggleButton.innerHTML = '<i class="fas fa-bars"></i>';
+
+const navbarLogo = document.createElement('div');
+navbarLogo.className = 'navbar-logo';
+navbarLogo.innerHTML = '<img id="logo-img" src="https://www.arenabreakout.wiki/images/logo.png" alt="Logo">';
+
+const navbarList = document.createElement('ul');
+navbarList.className = 'navbar-list';
+
+const homeItem = document.createElement('li');
+homeItem.className = 'navbar-item';
+homeItem.innerHTML = '<a href="https://www.arenabreakout.wiki/index.html">Home</a>';
+
+const directoryItem = document.createElement('li');
+directoryItem.className = 'navbar-item';
+directoryItem.innerHTML = '<a href="https://www.arenabreakout.wiki/content/directory/directory.html">Directory</a>';
+
+const aboutItem = document.createElement('li');
+aboutItem.className = 'navbar-item';
+aboutItem.innerHTML = '<a href="https://www.arenabreakout.wiki/content/documents/about.html">About</a>';
+
+navbarList.append(homeItem, directoryItem, aboutItem);
+
+navbar.append(toggleButton, navbarLogo, navbarList);
+navbarContainer.append(navbar);
+
 const burger = document.querySelector('.burger');
-const navbarList = document.querySelector('.navbar-list');
 
 burger.addEventListener('click', () => {
   navbarList.classList.toggle('active');
   burger.classList.toggle('active');
 });
+
 
 
 const closeButton = document.querySelector('.close-button');
@@ -38,3 +73,50 @@ liveButton.addEventListener('click', () => {
   closeButton.innerHTML = '<i class="fas fa-times"></i>';
   liveButton.style.display = 'none';
 });
+
+// reusableFooter.js
+
+const footerContainer = document.getElementById('footer-container');
+
+const footer = document.createElement('footer');
+footer.className = 'footer';
+
+const footerContent = document.createElement('div');
+footerContent.className = 'footer-content';
+
+const footerLogo = document.createElement('div');
+footerLogo.className = 'footer-logo';
+footerLogo.innerHTML = '<img src="../../images/logo.png" alt="Logo">';
+
+const footerText = document.createElement('p');
+footerText.textContent = '© 2023 Arena Breakout WIKI. NoCapxGaming. All Rights Reserved.';
+
+const footerSocialIcons = document.createElement('div');
+footerSocialIcons.className = 'footer-social-icons';
+footerSocialIcons.innerHTML = `
+  <a href="https://www.facebook.com/NocapxGaming"><i class="fab fa-facebook"></i></a>
+  <a href="https://www.instagram.com/nocapxgaming/"><i class="fab fa-instagram"></i></a>
+  <a href="https://twitter.com/NoCapxGaming"><i class="fab fa-twitter"></i></a>
+  <a href="https://www.youtube.com/channel/UCK_704Q4iCZ9acod6XQlu5A"><i class="fab fa-youtube"></i></a>
+`;
+
+const footerList = document.createElement('ul');
+footerList.className = 'footer-list';
+
+const contactUs = document.createElement('li');
+contactUs.className = 'footer-item';
+contactUs.innerHTML = '<a href="#">Contactanos</a>';
+
+const termsOfService = document.createElement('li');
+termsOfService.className = 'footer-item';
+termsOfService.innerHTML = '<a href="#">Terminos de Servicio</a>';
+
+const privacyPolicy = document.createElement('li');
+privacyPolicy.className = 'footer-item';
+privacyPolicy.innerHTML = '<a href="#">Privacy Policy</a>';
+
+footerList.append(contactUs, termsOfService, privacyPolicy);
+
+footerContent.append(footerLogo, footerText, footerSocialIcons, footerList);
+footer.append(footerContent);
+footerContainer.append(footer);
